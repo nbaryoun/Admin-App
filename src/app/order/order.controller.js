a4b026583479a435b6523f94eaa86b96653fd4f7
Noraen Baryoun <n.baryoun@gmail.com>
2017-02-07 12:17:15 -0500

    order component added but not finished

---
+++ src/app/order/order.controller.js
@@ -0,0 +1,21 @@
+/**
+ * Created by mohamedisse on 9/22/16.
+ */
+(function() {
+  'use strict';
+
+  angular
+    .module('marketingMap')
+    .controller('OrderController', OrderController);
+
+  /** @ngInject */
+  function OrderController($timeout, toastr) {
+    var order = this;
+
+    order.mapCreated = function (map) {
+      console.log('the map was created');
+      order.map = map;
+    }
+
+  }
+})();
