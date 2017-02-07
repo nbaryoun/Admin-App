5032af283f1b759d148baa7c162a51ad38643542
Noraen Baryoun <n.baryoun@gmail.com>
2017-02-07 11:17:40 -0500

    heatmapbutton replication

---
+++ src/app/components/heatmapbutton/heatmapbutton.directive.js
@@ -0,0 +1,30 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('marketingMap')
+    .directive('heatMapButton', heatMapButton);
+
+  /** @ngInject */
+  function heatMapButton() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'app/components/heatmapbutton/heatmapbutton.html',
+      scope: {
+      },
+      controller: HeatMapButtonController,
+      controllerAs: 'hm',
+      bindToController: true
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function HeatMapButtonController() {
+      var hm = this; //
+      var heatmapData = []
+
+    }
+  }
+
+})();
---
+++ src/app/components/heatmapbutton/heatmapbutton.directive.spec.js
@@ -0,0 +1,37 @@
+(function() {
+  'use strict';
+
+  /**
+   * @todo Complete the test
+   * This example is not perfect.
+   * Test should check if MomentJS have been called
+   */
+  describe('directive heatMapButton', function() {
+    // var $window;
+    var hm;
+    var el;
+    var scope;
+
+    beforeEach(module('marketingMap'));
+    beforeEach(inject(function($compile, $rootScope) {
+
+      scope = $rootScope.$new();
+
+      el  = angular.element('<heat-map-button></heat-map-button>');
+
+      $compile(el)(scope);
+      scope.$digest();
+      hm = el.isolateScope().hm;
+
+    }));
+
+    it('should be compiled', function() {
+      expect(el.html()).not.toEqual(null);
+    });
+
+    it('should have isolate scope object with instanciate members', function() {
+      expect(hm).toEqual(jasmine.any(Object));
+
+    });
+  });
+})();
