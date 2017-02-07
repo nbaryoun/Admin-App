3df5e6ff21dabc0d71475b25b29c64024d7b67e3
Noraen Baryoun <n.baryoun@gmail.com>
2017-02-07 12:25:49 -0500

    vendors stuff added

---
+++ src/app/vendors/vendors.controller.js
@@ -0,0 +1,58 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('marketingMap')
+    .controller('VendorsController', VendorsController);
+
+  /** @ngInject */
+  function VendorsController($timeout, toastr, sampleData) {
+    var vendors = this;
+    //creates map then loads markers
+    vendors.mapCreated = function (map) {
+      vendors.map = map;
+      vendors.loadMarkers();
+    };
+
+
+    //loads the marks
+    vendors.loadMarkers = function () {
+      vendors.data.forEach(function(vendor){
+        vendor.markers = [];
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(vendor.Geolocation.CurrentLat,vendor.Geolocation.CurrentLong),
+          map: vendors.map,
+          title: vendor.Place_id,
+          data: vendor
+        });
+        //info window content
+        var content = '<div>'+
+          'Vendor Name: ' + vendor.VendorName
+          +'<br>'+
+          'Vendor Location: ' + vendor.VendorAddress
+          +'<br>'+
+          'Number of Orders: ' + vendor.Num_of_Orders
+          +'<br>'+
+          'Money Spent: ' + vendor.Money_Spent +
+          '</div>';
+
+        var infoWindow = new google.maps.InfoWindow({
+          content: content
+        });
+
+        marker.addListener('click', function() {
+          infoWindow.open(vendors.map, marker);
+        });
+
+        vendor.markers.push(marker);
+        console.log(vendor.markers);
+      });
+    };
+
+    //gets vendor data from service
+    vendors.data = sampleData.getVendorData();
+
+  }
+
+
+})();
---
+++ src/app/vendors/vendors.controller.spec.js
@@ -0,0 +1,58 @@
+(function() {
+  'use strict';
+
+  describe('controllers', function(){
+    var vendors;
+    var $timeout;
+    var toastr;
+
+    beforeEach(module('marketingMap'));
+    beforeEach(inject(function(_$controller_, _$timeout_, _toastr_,sampleData) {
+      spyOn(_toastr_, 'info').and.callThrough();
+
+      vendors = _$controller_('VendorsController');
+      $timeout = _$timeout_;
+      toastr = _toastr_;
+    }));
+
+    describe('vendors.mapCreated', function () {
+      var data = 'this is a test';
+      beforeEach(inject(function () {
+        spyOn(users, 'loadMarkers');
+        vendors.mapCreated(data);
+      }));
+      it('should assign data to users.map', function () {
+        expect(vendors.map).toEqual(data);
+      });
+      it('should call users.loadMarkers', function () {
+        expect(vendors.loadMarkers).toHaveBeenCalled();
+      });
+    });
+    //TODO: Finish the vendor controller test
+    describe('vendors.loadMarkers', function() {
+      beforeEach(inject(function() {
+        vendors.loadMarkers();
+      }));
+      it('needs to load markers into the markers array for Each user', function() {
+        expect(users.Users[0].markers).toEqual(jasmine.any(Array));
+        expect(users.Users[0].markers.length).toEqual(users.Users[0].actions.length);
+      });
+      it('should add the data for each action to a marker', function () {
+        users.Users.forEach(function (user) {
+          user.markers.forEach(function (marker, index) {
+            expect(marker.data).toEqual(user.actions[index]);
+          });
+        });
+      });
+      it('each marker should be attached to the proper map', function() {
+        users.Users.forEach(function (user) {
+          user.markers.forEach(function (marker) {
+            expect(marker.map).toEqual(users.map);
+          });
+        });
+      });
+    });
+
+
+  });
+})();
